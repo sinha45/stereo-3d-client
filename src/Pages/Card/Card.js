275a9c18efb19570228e913f6a34c7646893673e
sinha45 <alfa45leonidas@gmail.com>
2022-12-23 23:25:18 +0600

    card and search fixed

--- src/Pages/Card/Card.js
+++ src/Pages/Card/Card.js
@@ -2,18 +2,90 @@ import React from 'react';
 
 const Card = () => {
     return (
-        <div className='ml-4 mr-4 mt-8 mb-8'>
-            <div className="card w-96 bg-base-100 shadow-xl ">
-                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">
-                        Shoes!
-                        <div className="badge badge-secondary">NEW</div>
-                    </h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
+        <div className='mt-20 mr-28 ml-44 mb-20'>
+            <div className='grid grid-cols-3 gap-4'>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Shoes!
+                            <div className="badge badge-secondary">NEW</div>
+                        </h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <div className="badge badge-outline">Fashion</div>
+                            <div className="badge badge-outline">Products</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl ">
+                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Shoes!
+                            <div className="badge badge-secondary">NEW</div>
+                        </h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <div className="badge badge-outline">Fashion</div>
+                            <div className="badge badge-outline">Products</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl ">
+                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Shoes!
+                            <div className="badge badge-secondary">NEW</div>
+                        </h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <div className="badge badge-outline">Fashion</div>
+                            <div className="badge badge-outline">Products</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl ">
+                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Shoes!
+                            <div className="badge badge-secondary">NEW</div>
+                        </h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <div className="badge badge-outline">Fashion</div>
+                            <div className="badge badge-outline">Products</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl ">
+                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Shoes!
+                            <div className="badge badge-secondary">NEW</div>
+                        </h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <div className="badge badge-outline">Fashion</div>
+                            <div className="badge badge-outline">Products</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl ">
+                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Shoes!
+                            <div className="badge badge-secondary">NEW</div>
+                        </h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <div className="badge badge-outline">Fashion</div>
+                            <div className="badge badge-outline">Products</div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -5,18 +5,32 @@ import Card from '../../Card/Card';
 const Home = () => {
     return (
         <>
-            <div className="hero min-h-[50%]" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
-                <div className="hero-overlay"></div>
-                <div className="hero-content text-center text-neutral-content">
-                    <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+            <div className="w-full h-96 bg-no-repeat bg-cover" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
+
+
+
+
+                <div className='flex flex-col justify-center items-center'>
+                    <div className='mt-24'>
+                        <h1 className='text-3xl font-semibold text-white'>The Best Platform For 3D Content Enthusiast</h1>
                     </div>
 
+                    <div className="form-control mt-6">
+                        <div className="input-group">
+
+                            <input type="text" placeholder="Search…" className="input input-bordered" />
+                            <button className="btn btn-square">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                            </button>
+
+                        </div>
+
+
+                    </div>
                 </div>
 
             </div>
+
             <Card></Card>
         </>
 
